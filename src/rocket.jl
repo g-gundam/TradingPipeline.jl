a59a7207cb60bea9cf70b18e4bccc24444387b9a
g-gundam <gg@nowhere>
2024-12-16 11:49:22 -0800

    Add struct ChartSubject

--- src/rocket.jl
+++ src/rocket.jl
@@ -12,3 +12,40 @@ function df_candles_observable(df::DataFrame)
         row.v
     ), eachrow(df)))
 end
+
+
+
+mutable struct ChartSubject <: Rocket.AbstractSubject{Candle}
+    charts::Dict{Symbol,Chart}
+    subscribers::Vector
+end
+
+function Rocket.on_subscribe!(subject::ChartSubject, actor)
+    push!(subject.subscribers, actor)
+    return voidTeardown
+end
+
+function Rocket.on_next!(subject::ChartSubject, c::Candle)
+    for (k, v) in subject.charts
+        complete_candle = TechnicalIndicatorCharts.update!(v, c)
+        if !isnothing(complete_candle)
+            # send a value of type Tuple{Symbol, Candle)} to subscribers
+            #next!(subject, (k, complete_candle))
+            for s in subject.subscribers
+                next!(s, (k, complete_candle))
+                #next!(s, complete_candle)
+                #next!(s, complete_candle.o)
+            end
+            yield() # INFO: This allows ExchangeFillSubject to have a chance to work.
+            # Otherwise, the loop goes too fast.
+        end
+    end
+end
+
+function Rocket.on_complete!(subject::ChartSubject)
+    @info :complete message=typeof(subject)
+end
+
+function Base.getindex(subject::ChartSubject, k::Symbol)
+    return subject.charts[k]
+end
