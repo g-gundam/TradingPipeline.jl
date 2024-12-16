a98a3361a5f1e75f80a036d660578a00fd28e529
Documenter.jl <documenter@juliadocs.github.io>
2024-12-16 08:35:08 +0000

    build based on b0f5720

---
+++ v0.1.0-DEV/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TradingPipeline","category":"page"},{"location":"#TradingPipeline","page":"Home","title":"TradingPipeline","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TradingPipeline.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TradingPipeline]","category":"page"}]
+}
