f3253bc39b691c74a889dd3c8357d5368295597c
g-gundam <g-gundam@users.noreply.github.com>
2024-12-18 07:09:05 -0800

    Explicitly load deps up top
    It's nice to see them all in one place.
    The includes have `using` statements inside them, but they
    shouldn't be needed anymore.

--- src/TradingPipeline.jl
+++ src/TradingPipeline.jl
@@ -1,6 +1,22 @@
 module TradingPipeline
-using DocStringExtensions
+
+# from the wider ecosystem
+using Chain
+using DataFrames
 using Dates
+using DocStringExtensions
+using EnumX
+using Printf
+
+# work of people I've interacted with
+import HierarchicalStateMachines as HSM
+using OnlineTechnicalIndicators
+
+# my own work
+using CryptoMarketData
+using TechnicalIndicatorCharts
+using ReversedSeries
+import ExchangeOperations as XO
 
 include("pnl.jl")
 include("report.jl")
