f0eed9790e66d145c20ed6b260c6fc736569b438
g-gundam <gg@nowhere>
2024-12-17 17:08:47 -0800

    Implement @hsm macro to fully instantiate the MarketOrderStrategyStateMachine

--- src/hsm_types.jl
+++ src/hsm_types.jl
@@ -36,3 +36,86 @@ struct OpenLongSignal <: HSM.AbstractHsmEvent end
 struct CloseLongSignal <: HSM.AbstractHsmEvent end
 struct OpenShortSignal <: HSM.AbstractHsmEvent end
 struct CloseShortSignal <: HSM.AbstractHsmEvent end
+
+const TP = TradingPipeline
+
+"""    @hsm strategy_subject
+
+Given a StrategySubject instance, define the states and transitions for a
+MarketOrderStrategyStateMachine, and return the singleton instance of that state machine.
+"""
+macro hsm(strategy_subject::AbstractSubject)
+    return quote
+        local strategy_subject = $(esc(strategy_subject))
+        local TP = TradingPipeline
+        local HSM = HierarchicalStateMachines
+
+        hsm                 = TP.MarketOrderStrategyStateMachine(nothing, strategy_subject)
+        neutral             = TP.Neutral(hsm, strategy_subject)
+        want_to_long        = TP.WantToLong(hsm, strategy_subject)
+        in_long             = TP.InLong(hsm, strategy_subject)
+        want_to_close_long  = TP.WantToCloseLong(hsm, strategy_subject)
+        want_to_short       = TP.WantToShort(hsm, strategy_subject)
+        in_short            = TP.InShort(hsm, strategy_subject)
+        want_to_close_short = TP.WantToCloseShort(hsm, strategy_subject)
+
+        function HSM.on_initialize!(state::TP.MarketOrderStrategyStateMachine)
+            HSM.transition_to_state!(hsm, neutral)
+        end
+
+        # define transitions
+        function HSM.on_event!(state::TP.Neutral, event::TP.OpenLongSignal)
+            HSM.transition_to_state!(hsm, want_to_long)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.WantToLong, event::TP.Fill)
+            HSM.transition_to_state!(hsm, in_long)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.InLong, event::TP.CloseLongSignal)
+            HSM.transition_to_state!(hsm, want_to_close_long)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.InLong, event::TP.StopFill)
+            HSM.transition_to_state!(hsm, neutral)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.WantToCloseLong, event::TP.Fill)
+            HSM.transition_to_state!(hsm, neutral)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.Neutral, event::TP.OpenShortSignal)
+            HSM.transition_to_state!(hsm, want_to_short)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.WantToShort, event::TP.Fill)
+            HSM.transition_to_state!(hsm, in_short)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.InShort, event::TP.CloseShortSignal)
+            HSM.transition_to_state!(hsm, want_to_close_short)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.InShort, event::TP.StopFill)
+            HSM.transition_to_state!(hsm, neutral)
+            return true
+        end
+
+        function HSM.on_event!(state::TP.WantToCloseShort, event::TP.Fill)
+            HSM.transition_to_state!(hsm, neutral)
+            return true
+        end
+
+        hsm
+    end
+end
+
+export @hsm
