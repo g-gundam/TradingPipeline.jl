07beeae29a80b6c161ec14e8b5aaf6c663de7c45
g-gundam <g-gundam@users.noreply.github.com>
2024-12-20 14:11:39 -0800

    Export a few useful generic functions

--- src/TradingPipeline.jl
+++ src/TradingPipeline.jl
@@ -22,11 +22,14 @@ import ExchangeOperations as XO
 
 include("pnl.jl")
 include("report.jl")
+export report
 include("hsm_types.jl")
 include("abstract_strategy.jl")
 include("strategies/goldencross.jl")
 include("strategies/hma.jl")
+export load_strategy
 include("rocket.jl")
 include("pipeline.jl")
+export simulate
 
 end
