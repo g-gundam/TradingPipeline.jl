5e56840fbd8d0f82e2052a9bdd54f6691ca612c1
g-gundam <g-gundam@users.noreply.github.com>
2024-12-21 01:00:51 -0800

    Don't test on 1.6

--- src/TradingPipeline.jl
+++ src/TradingPipeline.jl
@@ -33,3 +33,31 @@ include("pipeline.jl")
 export simulate
 
 end
+
+
+
+# REPL work
+
+if false
+    
+    using CryptoMarketData
+    using TechnicalIndicatorCharts
+    using ReversedSeries
+    import ExchangeOperations as XO
+
+    using UnPack
+    using LightweightCharts
+
+    pancakeswap = PancakeSwap()
+    btcusd1m = load(pancakeswap, "BTCUSD"; span=Date("2023-07-01"):Date("2024-11-29"))
+
+    import TradingPipeline as TP
+    import HierarchicalStateMachines as HSM
+    using TradingPipeline
+    using TradingPipeline: simulate, HMAStrategy, df_candles_observable, @hsm
+    using TradingPipeline: load_strategy, report
+
+    candle_observable = df_candles_observable(btcusd1m)
+    #@unpack hsm, simulator_session, chart_subject = simulate(candle_observable, HMAStrategy);
+
+end
