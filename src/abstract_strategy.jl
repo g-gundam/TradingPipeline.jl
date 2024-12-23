48ac08ecdb8910dda925bafe01909d669e4808e1
g-gundam <g-gundam@users.noreply.github.com>
2024-12-22 19:52:26 -0800

    Use a more appropriate parameter name - s/subject/strategy/

--- src/abstract_strategy.jl
+++ src/abstract_strategy.jl
@@ -5,8 +5,8 @@ using ReversedSeries
 abstract type AbstractStrategy end
 
 # A strategy is only responsible for answering these four questions.
-should_open_long(subject::AbstractStrategy) = false
-should_close_long(subject::AbstractStrategy) = false
-should_open_short(subject::AbstractStrategy) = false
-should_close_short(subject::AbstractStrategy) = false
+should_open_long(strategy::AbstractStrategy) = false
+should_close_long(strategy::AbstractStrategy) = false
+should_open_short(strategy::AbstractStrategy) = false
+should_close_short(strategy::AbstractStrategy) = false
 
