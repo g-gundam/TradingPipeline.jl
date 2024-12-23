24bc33f82e4ca08e14f4355b99d4e0c3af606aa1
g-gundam <g-gundam@users.noreply.github.com>
2024-12-23 01:19:18 -0800

    First attempt at visualizing trades on top of a chart

--- src/report.jl
+++ src/report.jl
@@ -1,4 +1,9 @@
 using DataFrames
+using ReversedSeries
+using ReversedSeries: find_index
+using TechnicalIndicatorCharts
+using TechnicalIndicatorCharts: Chart
+using LightweightCharts
 import ExchangeOperations as XO
 
 function make_result(open::XO.SimulatorMarketBuyFill, close::XO.SimulatorMarketSellFill)
@@ -23,7 +28,7 @@ function make_result(open::XO.SimulatorMarketSellFill, close::XO.SimulatorMarket
             pnl         = r.profit_loss)
 end
 
-"""report(session::XO.SimulatorSession) -> Vector{Result}
+"""report(session::XO.SimulatorSession) -> DataFrame
 
 Return a list of trades that happened during the simulator session.
 """
@@ -54,3 +59,42 @@ function report(session::XO.SimulatorSession)
     end
     return DataFrame(trades)
 end
+
+"""$(TYPEDSIGNATURES)
+
+Let's see if I can visualize trades on top of a chart.
+"""
+function TechnicalIndicatorCharts.visualize(t::Tuple{Chart, XO.AbstractSession})
+    (chart, session) = t
+    rdf = report(session)
+    layout = TechnicalIndicatorCharts.visualize(chart)
+    cell11 = layout.panels["cell11"]
+    stix = cell11.charts[1]
+    index_of = ts::DateTime -> find_index(chart.df.ts, t -> t >= ts)
+    for row in eachrow(rdf)
+        index2 = if ismissing(index_of(row.close_ts))
+            lastindex(chart.df.ts)
+        else
+            index_of(row.close_ts)
+        end
+        trade_marker = lwc_trend_line(
+            index_of(row.open_ts),
+            row.open_price,
+            index2-1,
+            row.close_price;
+            line_color="#7CB518"
+        )
+        push!(stix.plugins, trade_marker)
+    end
+    return layout
+end
+
+#=
+layout = chart_subject[:trend] |> visualize
+cell11 = layout.panels["cell11"]
+typeof(cell11) <: LWCPanel
+cell11.charts
+stix = cell11.charts[1]                 # candlestick chart is here
+typeof(stix) <: LWCChart
+stix.plugins                            # Vector{LWCPlugin}
+=#
