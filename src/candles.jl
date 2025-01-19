a0777897ff74dd7ac21505292b9e5a96fa04def3
g-gundam <g-gundam@users.noreply.github.com>
2025-01-18 16:48:44 -0800

    Add stubs for more candle observables

--- src/candles.jl
+++ src/candles.jl
@@ -1,4 +1,16 @@
 using Rocket
+using CryptoMarketData
+using HTTP
+using HTTP: WebSocket, WebSockets
+
+# This file will be all about creating candle observables.
+# - The original kind was a Rocket.iterable backed by a DataFrame.
+# - I need to make a WebSocket backed candle observable too.
+# - I also want a hybrid of the two where it:
+#   + starts with a DataFrame
+#   + uses HTTP to fetch the most recent minutes (in case it doesn't have them from the df)
+#   + swithces to WebSockets
+# - I just realized the switch has to be lazy!
 
 # First, let's take a DataFrame and make it into an Observable.
 function df_candles_observable(df::DataFrame)
@@ -10,4 +22,10 @@ function df_candles_observable(df::DataFrame)
         row.c,
         row.v
     ), eachrow(df)))
+end
+
+function ws_candles_observable(bitstamp::CryptoMarketData.Bitstamp)
+end
+
+function ws_candles_observable(pancakeswap::CryptoMarketData.PancakeSwap)
 end
