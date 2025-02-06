5efcf078e3e47b9ba3d6288993bba6b663ebe3f2
g-gundam <g-gundam@users.noreply.github.com>
2025-02-06 05:54:21 -0800

    Rethinking stops
    What I had was too complicated.
    It can be drastically simplified.

--- src/abstract_stop.jl
+++ src/abstract_stop.jl
@@ -2,3 +2,14 @@ abstract type AbstractStop end
 
 # initial_stop
 # should_move_stop
+
+
+# XXX: REDESIGN
+# I don't need to use polymorphism here.
+# Functions may be enough to express variances in behavior.
+
+struct Stop
+    initial::Any,
+    break_even::Any,
+    move_when::Function
+end
