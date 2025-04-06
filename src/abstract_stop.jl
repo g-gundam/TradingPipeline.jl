7a5f6191e393ef66ece6654a1f28b3d83baa8c32
g-gundam <g-gundam@users.noreply.github.com>
2025-04-06 05:17:08 -0700

    Remove AbstractStop
    The polymorphism that felt appropriate for strategies doesn't feel
    appropriate for stop loss policies.  The control points feel even more
    fine grained.

--- src/abstract_stop.jl
+++
@@ -1,7 +0,0 @@
-# All stop policies are a subtype of AbstractStop
-abstract type AbstractStop end
-
-# What methods should be implemented by stop policies?
-should_move_stop(policy::AbstractStop) = false
-# TODO: I need something for initial price
-# initial_stop(policy::AbstractStop) = 
