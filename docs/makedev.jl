0a121336a0e068d918df1071db86b341590474f1
g-gundam <g-gundam@users.noreply.github.com>
2025-08-04 17:50:54 -0700

    Add instructions in the comments

--- docs/makedev.jl
+++ docs/makedev.jl
@@ -7,5 +7,13 @@ include("shared.jl")
 
 makedocs(; md_local...)
 
-#LiveServer.serve(dir = "docs/build/1")
+## This is all I need to run for local documentation development.
+#include("makedev.jl")
+#ls_pid = Threads.@spawn servedocs(foldername=pwd())
+
+## Somehow, this isn't needed.
+#DocumenterVitepress.dev_docs("build", md_output_path = "")
+
+## Kill the spawned thread like this.
+#schedule(ls_id, InterruptException(); error=true) # stop servedocs thread
 
